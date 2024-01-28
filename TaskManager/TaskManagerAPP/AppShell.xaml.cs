1f0303ababaf522b50bb9638f5e5024f92694ec2
Valerijs Viskers <viskersv@gmail.com>
2024-01-28 17:34:13 +0200

    adding app proj

---
+++ TaskManager/TaskManagerAPP/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace TaskManagerAPP
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ TaskManager/TaskManagerAPP/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace TaskManagerAPP
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
